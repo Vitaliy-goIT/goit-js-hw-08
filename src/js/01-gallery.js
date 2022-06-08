4774c86ac25fcf8262c89d6539a09a6c9a9dab01
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-06-09 00:26:49 +0300

    1task-done!

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryEl = createGalleryEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryEl);
+
+function createGalleryEl(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
