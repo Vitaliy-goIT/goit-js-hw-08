b462cfef0f5530d9f407140e7dcb71d88abebaf0
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-06-10 20:46:57 +0300

    2task - done!

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  const time = Math.round(data.seconds);
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
